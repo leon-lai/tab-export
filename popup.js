8d15feeb7fe12bba883940a031415fa567bc2499
leon-lai <Leon.L.Y.Lai@gmail.com>
2019-09-01 02:40:55 +0800

    Use Array.prototype.map, Array.prototype.filter, template literals

--- popup.js
+++ popup.js
@@ -2,21 +2,15 @@ window.addEventListener("load", () => {
 	const textarea_URLs = document.getElementById("textarea_URLs");
 	const button_openURLs = document.getElementById("button_openURLs");
 	button_openURLs.addEventListener("click", () => {
-		textarea_URLs.value.trim().split("\n").forEach(url => {
-			url=url.trim();
-			if(url!==""&&!url.startsWith("#")){
-				if(!/^[^:\.]*:[^\.]*\./.test(url)){
-					url="http:"+url;
-				}
+		textarea_URLs.value.split("\n")
+		.map(line => line.trim())
+		.filter(line => line && ! line.startsWith("#"))
+		.map(url => /^[^:\.]*:[^\.]*\./i.test(url) ? url : `http://${url}`)
+		.forEach(url => {
 				chrome.tabs.create({active:false,url:url});
-			}
 		});
 	});
 	chrome.tabs.query({}, tabs => {
-		var urls="";
-		tabs.forEach(function(tab){
-			urls+=tab.url+"\n";
-		});
-		textarea_URLs.value=urls;
+		textarea_URLs.value = tabs.map(tab => tab.url).join("\n");
 	});
 });
