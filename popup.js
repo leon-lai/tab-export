5182d7c92bbdc86d640421b50862ab2d09a3adef
leon-lai <Leon.L.Y.Lai@gmail.com>
2019-09-08 23:54:25 +0800

    Omit unnecessary stuff

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+addEventListener("load", () => {
 	const textarea_URLs = document.getElementById("textarea_URLs");
 	const button_openURLs = document.getElementById("button_openURLs");
 	button_openURLs.addEventListener("click", () => {
